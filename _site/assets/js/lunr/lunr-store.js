e3de8266a0307fa21697dc9b0110011c190af096
surapong.n <surapong.n@nida.ac.th>
2025-06-05 15:38:28 +0700

    update

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "example post 1",
+        "excerpt":"   บทความนี้เป็นบทความตัวอย่างที่เขียนเพื่อแสดงการใช้ Jekyll กับธีม Minimal Mistakes    หัวข้อ   นี่คือตัวอย่างเนื้อหาที่เขียนเป็นภาษาไทยได้สมบูรณ์  ","categories": ["blog"],
+        "tags": ["ตัวอย่าง","ภาษาไทย"],
+        "url": "/blog/2025/06/01/example-post-1.html",
+        "teaser": null
+      },{
+        "title": "example post 2",
+        "excerpt":"   บทความตัวอย่างที่สอง พร้อมโค้ดและสไตล์ที่สวยงาม    โค้ดตัวอย่าง   jekyll serve  ","categories": ["blog"],
+        "tags": ["github","jekyll"],
+        "url": "/blog/2025/06/02/example-post-2.html",
+        "teaser": null
+      },{
+        "title": "Linux LVM Management",
+        "excerpt":"Step by step to add disk to LVM and resize lv Add Disk fdisk -l /dev/sdx and create new partition pvcreate /dev/sdx1 vgdisplay Create LVM Volume vgcreate volumn_name /dev/sdd Extend Volume vgextend vol_group_name /dev/sdx1 Create Logical Volume lvcreate -l 20m -n logical_vol vol_group lvcreate -l 100%FREE -n logical_vol vol_group lvdisplay...","categories": ["blog"],
+        "tags": ["linux","lvm"],
+        "url": "/blog/2025/06/05/example-post-3.html",
+        "teaser": null
+      }]
